873ce3f52d062bc6f7d8cec71c1ee0fb2c0725e9
Koka <koka.tirkia@gmail.com>
2021-08-12 13:53:31 +0400

    Add extended weather item expandable functionality

--- app/src/main/java/com/example/weather/ui/components/ExtendedWeather.kt
+++ app/src/main/java/com/example/weather/ui/components/ExtendedWeather.kt
@@ -1,22 +1,21 @@
 package com.example.weather.ui.components
 
-import androidx.compose.foundation.Image
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberImagePainter
 import com.example.weather.ui.model.ExtendedWeatherUi
 import com.example.weather.ui.model.WeatherExtendedDataUi
-import com.example.weather.utils.Constants
 
+@ExperimentalAnimationApi
 @Composable
 fun ExtendedWeather(extendedWeatherState: ExtendedWeatherUi) {
     LazyColumn(
@@ -29,40 +28,34 @@ fun ExtendedWeather(extendedWeatherState: ExtendedWeatherUi) {
     }
 }
 
+@ExperimentalAnimationApi
 @Composable
 fun WeatherItem(weatherExtendedDataUi: WeatherExtendedDataUi) {
-    Box(
+    var isExpanded by remember { mutableStateOf(false) }
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .size(70.dp)
             .background(
                 color = MaterialTheme.colors.primary,
                 shape = RoundedCornerShape(size = 10.dp)
             )
-            .padding(10.dp),
+            .clickable(onClick = {
+                isExpanded = !isExpanded
+            })
+            .padding(5.dp)
     ) {
-        Text(
-            text = weatherExtendedDataUi.dt,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.TopStart)
-        )
-        Text(
-            text = weatherExtendedDataUi.weather[0].description,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.BottomStart)
-        )
-        Image(
-            painter = rememberImagePainter(Constants.iconUrl + weatherExtendedDataUi.weather[0].icon),
-            contentDescription = null,
-            modifier = Modifier
-                .padding(horizontal = 10.dp)
-                .size(35.dp)
-                .align(Alignment.TopEnd)
-        )
-        Text(
-            text = weatherExtendedDataUi.main.temp,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.BottomEnd)
-        )
+
+        WeatherItemMainBox(weatherExtendedDataUi)
+
+        AnimatedVisibility(visible = isExpanded) {
+            Column {
+                Divider(modifier = Modifier.padding(vertical = 10.dp))
+                PHWRow(
+                    pressure = weatherExtendedDataUi.main.pressure,
+                    humidity = weatherExtendedDataUi.main.humidity,
+                    windSpeed = weatherExtendedDataUi.wind.speed
+                )
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/weather/ui/components/WeatherItemMainBox.kt
@@ -0,0 +1,50 @@
+package com.example.weather.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import com.example.weather.ui.model.WeatherExtendedDataUi
+import com.example.weather.utils.Constants
+
+@Composable
+fun WeatherItemMainBox(weatherExtendedDataUi: WeatherExtendedDataUi) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .size(75.dp)
+            .padding(10.dp)
+    ) {
+        Text(
+            text = weatherExtendedDataUi.dt,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.TopStart)
+        )
+        Text(
+            text = weatherExtendedDataUi.weather[0].description,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.BottomStart)
+        )
+        Image(
+            painter = rememberImagePainter(Constants.iconUrl + weatherExtendedDataUi.weather[0].icon),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(horizontal = 10.dp)
+                .size(35.dp)
+                .align(Alignment.TopEnd)
+        )
+        Text(
+            text = weatherExtendedDataUi.main.temp,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.BottomEnd)
+        )
+    }
+}

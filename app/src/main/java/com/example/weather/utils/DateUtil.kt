22d31007d97f4d77317d5a3af8a2420ce92a3fdc
kokatirkia <koka.tirkia@gmail.com>
2023-01-14 18:51:18 +0400

    Change parameter name

--- app/src/main/java/com/example/weather/utils/DateUtil.kt
+++ app/src/main/java/com/example/weather/utils/DateUtil.kt
@@ -4,8 +4,8 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 object DateUtil {
-    fun longToString(time: Long, pattern: String): String = SimpleDateFormat(
+    fun longToString(timeInSeconds: Long, pattern: String): String = SimpleDateFormat(
         pattern,
         Locale.ENGLISH
-    ).format(Date(time * 1000))
+    ).format(Date(timeInSeconds * 1000))
 }

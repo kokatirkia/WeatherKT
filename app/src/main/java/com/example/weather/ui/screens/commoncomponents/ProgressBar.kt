063073f3860b9d46fd85818985b52605e1d8e845
kokatirkia <koka.tirkia@gmail.com>
2023-01-24 14:59:39 +0400

    Add contentDescription

--- app/src/main/java/com/example/weather/ui/screens/commoncomponents/ProgressBar.kt
+++ app/src/main/java/com/example/weather/ui/screens/commoncomponents/ProgressBar.kt
@@ -6,13 +6,17 @@ import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun CircularProgressBar() {
     Box(modifier = Modifier.fillMaxSize()) {
         CircularProgressIndicator(
-            modifier = Modifier.align(Alignment.Center),
+            modifier = Modifier
+                .align(Alignment.Center)
+                .semantics { contentDescription = "circularProgressIndicator" },
             strokeWidth = 5.dp
         )
     }

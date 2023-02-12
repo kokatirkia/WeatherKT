a57a03333a512222a2630d573efc4d0c87c5b3b4
kokatirkia <koka.tirkia@gmail.com>
2023-02-12 17:05:55 +0400

    Make fields private

--- app/src/test/java/com/example/weather/data/repository/WeatherApiFactory.kt
+++ app/src/test/java/com/example/weather/data/repository/WeatherApiFactory.kt
@@ -5,20 +5,20 @@ import com.example.weather.data.networking.model.ExtendedWeatherResponse
 import com.example.weather.data.networking.model.WeatherResponse
 
 object WeatherApiFactory {
-    const val main = "Clear"
-    const val description = "clear"
-    const val icon = "01n"
-    const val temp = 8.84
-    const val feelsLike = "8.84"
-    const val pressure = 1018
-    const val humidity = 49
-    const val speed = 8.23
-    const val dt = 1641391386L
-    const val dtTxt = "2023-02-11 18:00:00"
-    const val sunrise = 1641356867L
-    const val sunset = 1641390223L
-    const val name: String = "Tbilisi"
-    const val message = 0.0
+    private const val main = "Clear"
+    private const val description = "clear"
+    private const val icon = "01n"
+    private const val temp = 8.84
+    private const val feelsLike = "8.84"
+    private const val pressure = 1018
+    private const val humidity = 49
+    private const val speed = 8.23
+    private const val dt = 1641391386L
+    private const val dtTxt = "2023-02-11 18:00:00"
+    private const val sunrise = 1641356867L
+    private const val sunset = 1641390223L
+    private const val name: String = "Tbilisi"
+    private const val message = 0.0
 
     fun makeWeatherResponse() = WeatherResponse(
         makeCurrentWeatherResponse(),

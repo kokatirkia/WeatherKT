1975db977274f00b36686355d84d5a5cc2bc8720
kokatirkia <koka.tirkia@gmail.com>
2023-02-12 17:22:28 +0400

    Refactoring WeatherEntityFactory.kt

--- app/src/test/java/com/example/weather/data/repository/WeatherEntityFactory.kt
+++ app/src/test/java/com/example/weather/data/repository/WeatherEntityFactory.kt
@@ -3,55 +3,39 @@ package com.example.weather.data.repository
 import com.example.weather.data.localdatabase.model.CurrentWeatherEntity
 import com.example.weather.data.localdatabase.model.ExtendedWeatherEntity
 import com.example.weather.data.localdatabase.model.WeatherEntity
-import java.util.*
-import java.util.concurrent.ThreadLocalRandom
 
 object WeatherEntityFactory {
-
-    private fun makeRandomString() = UUID.randomUUID().toString()
-
-    private fun makeRandomInt() =
-        ThreadLocalRandom.current().nextInt(0, 1000 + 1)
-
-    private fun makeRandomDouble() =
-        ThreadLocalRandom.current().nextDouble(0.0, 1000.0 + 1.0)
-
-    private fun makeRandomLong() =
-        ThreadLocalRandom.current().nextLong(0L, 1000L + 1L)
+    private const val main = "Clear"
+    private const val description = "clear"
+    private const val icon = "01n"
+    private const val temp = 8.84
+    private const val feelsLike = "8.84"
+    private const val pressure = 1018
+    private const val humidity = 49
+    private const val speed = 8.23
+    private const val dt = 1641391386L
+    private const val dtTxt = "2023-02-11 18:00:00"
+    private const val sunrise = 1641356867L
+    private const val sunset = 1641390223L
+    private const val name: String = "Tbilisi"
 
     fun makeWeatherEntity() = WeatherEntity(
-        id = 1,
         currentWeather = CurrentWeatherEntity(
-            listOf(
-                CurrentWeatherEntity.Weather(
-                    makeRandomString(),
-                    makeRandomString(),
-                    makeRandomString()
-                )
-            ),
-            CurrentWeatherEntity.Main(
-                makeRandomDouble(),
-                makeRandomString(),
-                makeRandomInt(),
-                makeRandomInt()
-            ),
-            CurrentWeatherEntity.Wind(makeRandomDouble()),
-            CurrentWeatherEntity.Sys(makeRandomLong(), makeRandomLong()),
-            makeRandomLong(),
-            makeRandomString()
+            listOf(CurrentWeatherEntity.Weather(main, description, icon)),
+            CurrentWeatherEntity.Main(temp, feelsLike, pressure, humidity),
+            CurrentWeatherEntity.Wind(speed),
+            CurrentWeatherEntity.Sys(sunrise, sunset),
+            dt,
+            name
         ),
         extendedWeather = ExtendedWeatherEntity(
             listOf(
                 ExtendedWeatherEntity.WeatherItem(
-                    makeRandomLong(),
-                    ExtendedWeatherEntity.Main(
-                        makeRandomDouble(),
-                        makeRandomInt(),
-                        makeRandomInt()
-                    ),
-                    makeRandomString(),
-                    listOf(ExtendedWeatherEntity.Weather(makeRandomString(), makeRandomString())),
-                    ExtendedWeatherEntity.Wind(makeRandomDouble())
+                    dt,
+                    ExtendedWeatherEntity.Main(temp, pressure, humidity),
+                    dtTxt,
+                    listOf(ExtendedWeatherEntity.Weather(description, icon)),
+                    ExtendedWeatherEntity.Wind(speed)
                 )
             )
         )
